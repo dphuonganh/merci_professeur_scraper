53f3fdc81ac1bd970ee6163b3c4e8eb391223b24
nhuong <nhuong>
2019-08-21 23:25:37 +0700

    done wp2

--- Episode.py
+++ Episode.py
@@ -1,4 +1,5 @@
-import json;
+import json
+import requests
 
 # Waypoint 1: Write a Python Class Episode
 class Episode: # _ co the gan duoc luc test print title = "a"
@@ -32,9 +33,10 @@ class Episode: # _ co the gan duoc luc test print title = "a"
 
     @staticmethod
     def from_json(payload):
-        return Episode(payload['title'], payload["url"], payload["image"], payload["date"], payload["duration"])
+        return Episode(payload['title'], payload["url"], payload["image"],\
+        payload["date"], payload["duration"])
 
-    # Waypoint 2: Retrieve the Identification of an Episode
+# Waypoint 2: Retrieve the Identification of an Episode
     @staticmethod
     def __parse_episode_id(url):
         return url.split('.')[2].split('/')[3]
@@ -44,12 +46,12 @@ class Episode: # _ co the gan duoc luc test print title = "a"
         self._episode_id = self.__parse_episode_id(self._image_url)
         return self._episode_id
 
-# TEST
 with open('./merci-professeur.json', 'r') as myfile:
     data = myfile.read()
 payload = json.loads(data)
 payload_0 = payload['episodes'][0]
 a = Episode.from_json(payload_0)
+# TEST WP1
 # print (a.title)
 # print (a.page_url)
 # print (a.image_url)
@@ -57,4 +59,23 @@ a = Episode.from_json(payload_0)
 # print (a.duration)
 # a.title = 'sth else'
 # print (a.title)
-print (a.episode_id)
+
+# TEST WP2
+# print (a.episode_id)
+
+
+# Waypoint 3: Fetch the List of Episodes
+def read_url(url, maximum_attempt_count=3,
+    sleep_duration_between_attempts=10):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0)\
+    Gecko/20100101 Firefox/46.0'}
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+
+def fetch_episodes(url):
+    return (read_url(url))
+
+
+url = 'http://www.tv5monde.com/emissions/episodes/merci-professeur.json'
+print(fetch_episodes(url))
